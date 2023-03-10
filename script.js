ac6cbae07a8019a5b44db99745c16f1d65cdae82
Roman <95614494+DidunR@users.noreply.github.com>
2023-03-10 18:33:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+//2
+console.log("Didun");
+
+//3
+let firstAnimal = "dog";
+let secondAnimal = "cat";
+console.log(firstAnimal);
+console.log(secondAnimal);
+firstAnimal = secondAnimal;
+console.log(firstAnimal);
+console.log(secondAnimal);
+
+//4
+let teslaCar = {
+    model: "Y",
+    color: "black",
+    price: 60000,
+    electric: true,
+    power: undefined,
+    year: null
+};
+console.log(teslaCar);
+
+//5
+const areYouAdult = confirm("Are you adult?");
+console.log(areYouAdult);
+const isAdult = areYouAdult;
+console.log(isAdult);
+
+//6
+const myName = "Roman";
+const surname = "Didun";
+const group = 1;
+const yearOfBirth = 1989;
+const merried = true;
+console.log(typeof yearOfBirth, yearOfBirth);
+console.log(typeof group, group);
+console.log(typeof merried, merried);
+console.log(typeof myName, myName);
+console.log(typeof surname, surname);
+const fish = undefined;
+const animal = null;
+console.log(fish);
+console.log(animal);
+
+//7
+const yourName = prompt("What is your name?");
+const yourEmail = prompt("What is your e-mail?");
+const yourParol = prompt("What is your parol?");
+let info = "'Dear " + yourName + ", your email is " + yourEmail + ", your password is " + yourParol + "'";
+alert(info);
+
+//8
+let secondInMinute = 60;
+let minuteInHour = 60;
+let secondInHour = secondInMinute * minuteInHour;
+let hourInDay = 24;
+let secondInDay = secondInHour * hourInDay;
+let dayInMonth = 30;
+let secondInMonth = secondInDay * dayInMonth;
+console.log(secondInHour);
+console.log(secondInDay);
+console.log(secondInMonth);
+
